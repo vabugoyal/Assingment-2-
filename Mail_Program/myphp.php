fb38052e97a2af6fb8d08a6afe7495dced3888f3
Vaibhav <vaibhavgoyalop@gmail.com>
2021-08-26 18:03:24 +0530

    this is the filnal before php thing

---
+++ Mail_Program/myphp.php
@@ -0,0 +1,11 @@
+<?php 
+echo "The control came here.";
+// $first_name = $_POST["first_name"];
+// $last_name = $_POST["last_name"];
+// $email_address = $_POST["email_address"];
+// $telephone = $_POST["telephone"];
+// $comments = $_POST["comments"];
+// echo ($_POST["first_name"]);
+// mail("vaibhavgoyalop@gmail.com", "Testing", "This worked properly");
+
+?>
